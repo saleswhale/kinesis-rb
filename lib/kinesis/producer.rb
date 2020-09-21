176c62a7534e500c586a0a34df80ed381fea952d
odina abellana <odina.abellana@gmail.com>
2020-09-21 13:26:35 +0800

    Kinesis::Producer - Initial implementation

--- lib/kinesis/producer.rb
+++ lib/kinesis/producer.rb
@@ -1,10 +1,91 @@
 # frozen_string_literal: true
 
 module Kinesis
+  # Kinesis::AsyncProducer
+  class AsyncProducer < SubthreadLoop
+    # * The maximum size of a data blob (the data payload before base64-encoding) is up to 1 MB.
+    # * Each shard can support up to 1,000 records per second for writes, up to a maximum total data write rate of 1 MB
+    #   per second (including partition keys).
+    # * PutRecords supports up to 500 records in a single call
+    MAX_RECORDS_SIZE = (2**20)
+    MAX_RECORDS_COUNT = 500
+
+    def initialize(stream_name:, buffer_time:, record_queue:)
+      @buffer_time = buffer_time
+      @record_queue = record_queue
+      @stream_name = stream_name
+    end
+
+    def preprocess
+      @alive = true
+      @kinesis_client = Aws::Kinesis::Client.new
+      @next_record_queue = Queue.new
+      @record_count = 0
+      @record_size = 0
+      @retries = 0
+    end
+
+    def process
+      while @alive
+        next if @record_queue.empty?
+
+        # record, explicit_hash_key, partition_key = @record_queue.pop
+        record = @record_queue.pop
+
+        record = {
+          'Data': record
+        } # TODO: Add PartitionKey
+
+        @record_size += sizeof(record)
+
+        if @record_size >= MAX_RECORDS_SIZE
+          # Log: Records exceed MAX_RECORDS_SIZE (#{MAX_RECORDS_SIZE})! Adding to next_records: #{record}
+          @next_record_queue << record
+          break
+        end
+
+        @record_queue << record
+        @record_count += 1
+
+        if @record_count >= MAX_RECORDS_COUNT
+          # Log: Records have reached MAX_RECORDS_COUNT (#{MAX_RECORDS_COUNT})! Flushing records
+          break
+        end
+      end
+
+      flush_records
+      0 # sleep_time
+    end
+
+    def flush_records
+      return if @record_queue.empty?
+
+      # Log: Flushing #{@record_queue.size} records
+
+      records = @record_queue.size.times { @record_queue.pop }
+
+      @kinesis_client.put_records(
+        records: records,
+        stream_name: @stream_name
+      )
+
+      @next_record_queue.size.times { @record_queue << @next_record_queue.pop }
+    end
+  end
+
   # Kinesis::Producer
   class Producer
-    def initialize
-      # TODO
+    def initialize(stream_name:, buffer_time: 0.5)
+      @record_queue = Queue.new
+      @async_producer = AsyncProducer.new(
+        record_queue: @record_queue,
+        stream_name: stream_name,
+        buffer_time: buffer_time
+      )
+    end
+
+    def put(data)
+      @record_queue << data
     end
   end
 end
