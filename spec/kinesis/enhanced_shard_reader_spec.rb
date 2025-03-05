045979a63e7bedaffeb6bcc3f0c7767465178d59
HiiYL <yonglian146@gmail.com>
2025-03-05 12:29:16 +0800

    add more tests

--- spec/kinesis/enhanced_shard_reader_spec.rb
+++ spec/kinesis/enhanced_shard_reader_spec.rb
@@ -4,10 +4,11 @@ require 'rspec'
 require 'spec_helper'
 require 'kinesis/enhanced_shard_reader'
 
+# rubocop:disable Metrics/BlockLength
 describe Kinesis::EnhancedShardReader do
   let(:error_queue) { Queue.new }
   let(:record_queue) { Queue.new }
-  let(:logger) { instance_double('Logger', info: nil, error: nil) }
+  let(:logger) { instance_double('Logger', info: nil, error: nil, warn: nil) }
   let(:kinesis_client) { instance_double(Aws::Kinesis::Client) }
   let(:subscription) { instance_double('Aws::Kinesis::SubscribeToShardOutput', close: nil) }
   let(:event_stream) { instance_double('Aws::Kinesis::EventStream') }
@@ -24,7 +25,11 @@ describe Kinesis::EnhancedShardReader do
     allow(subscription).to receive(:on_event_stream).and_yield(event_stream)
     allow(event_stream).to receive(:on_record_event)
     allow(event_stream).to receive(:on_error_event)
-    allow(subscription).to receive(:wait)
+
+    # Mock wait to avoid the actual blocking behavior
+    allow(subscription).to receive(:wait) do
+      # Don't actually wait or raise in tests
+    end
   end
 
   subject do
@@ -75,4 +80,56 @@ describe Kinesis::EnhancedShardReader do
       expect(record_queue.pop).to eq(['test-shard-id', record])
     end
   end
+
+  describe '#wait_for_events' do
+    it 'waits for events from the subscription' do
+      # Set up the subscription
+      subject.instance_variable_set(:@subscription, subscription)
+
+      # Expect wait to be called
+      expect(subscription).to receive(:wait)
+
+      # Call the method but catch the expected exception
+      expect do
+        subject.send(:wait_for_events)
+      end.to raise_error(StandardError, /Subscription ended/)
+    end
+  end
+
+  describe 'error handling' do
+    before do
+      subject.instance_variable_set(:@subscription, subscription)
+    end
+
+    it 'handles JSON parsing errors' do
+      error = JSON::ParserError.new('unexpected token')
+      allow(kinesis_client).to receive(:subscribe_to_shard).and_raise(error)
+
+      expect(logger).to receive(:error).at_least(:once)
+      expect(error_queue).to receive(:<<).with(error)
+
+      subject.send(:process)
+    end
+
+    it 'handles AWS service errors' do
+      error = Aws::Kinesis::Errors::ResourceNotFoundException.new(nil, 'Resource not found')
+      allow(kinesis_client).to receive(:subscribe_to_shard).and_raise(error)
+
+      expect(logger).to receive(:error).at_least(:once)
+      expect(error_queue).to receive(:<<).with(error)
+
+      subject.send(:process)
+    end
+
+    it 'handles general errors' do
+      error = StandardError.new('General error')
+      allow(kinesis_client).to receive(:subscribe_to_shard).and_raise(error)
+
+      expect(logger).to receive(:error).at_least(:once)
+      expect(error_queue).to receive(:<<).with(error)
+
+      subject.send(:process)
+    end
+  end
 end
+# rubocop:enable Metrics/BlockLength
