36bd45b1a8b717fb6ee0e0c74bbe2949609fa861
HiiYL <yonglian146@gmail.com>
2025-03-05 12:24:45 +0800

    refactor, add more logging

--- lib/kinesis/enhanced_shard_reader.rb
+++ lib/kinesis/enhanced_shard_reader.rb
@@ -51,35 +51,88 @@ module Kinesis
     end
 
     def read_with_enhanced_fan_out
+      log_start_info
+
+      begin
+        create_subscription
+        setup_event_handlers
+        wait_for_events
+      rescue JSON::ParserError => e
+        handle_json_error(e)
+      rescue Aws::Errors::ServiceError => e
+        handle_aws_error(e)
+      rescue StandardError => e
+        handle_general_error(e)
+      end
+
+      @sleep_time # Return sleep time for retry
+    end
+
+    def log_start_info
+      @logger.info("Starting enhanced fan-out for shard #{@shard_id} with consumer ARN: #{@consumer_arn}")
+      @logger.info("Starting position: #{@starting_position.inspect}")
+    end
+
+    def create_subscription
       @subscription = @kinesis_client.subscribe_to_shard(
         consumer_arn: @consumer_arn,
         shard_id: @shard_id,
         starting_position: @starting_position
       )
 
+      @logger.info("Successfully created subscription for shard #{@shard_id}")
+    end
+
+    def setup_event_handlers
       @subscription.on_event_stream do |event_stream|
+        @logger.info("Setting up event handlers for shard #{@shard_id}")
+
         event_stream.on_record_event do |event|
           process_records(event.records)
         end
 
         event_stream.on_error_event do |event|
+          error_message = "Error in enhanced fan-out subscription for shard #{@shard_id}: #{event.error.message}"
+          @logger.error(error_message)
           @error_queue << event.error
-          @logger.error("Error in enhanced fan-out subscription for shard #{@shard_id}: #{event.error.message}")
         end
       end
+    end
 
-      # This will block until the subscription is closed
+    def wait_for_events
+      @logger.info("Waiting for events on shard #{@shard_id}")
       @subscription.wait
 
       # After subscription ends, we need to raise an error to trigger a retry
+      @logger.warn("Subscription ended for shard #{@shard_id}, will retry")
       raise StandardError, "Subscription ended for shard #{@shard_id}, will retry"
-    rescue StandardError => e
-      @error_queue << e
-      @logger.error("Error setting up enhanced fan-out for shard #{@shard_id}: #{e.message}")
-      @sleep_time # Return sleep time for retry
+    end
+
+    def handle_json_error(error)
+      error_message = "JSON parsing error in enhanced fan-out for shard #{@shard_id}: #{error.message}"
+      @logger.error(error_message)
+      @logger.error("Consumer ARN: #{@consumer_arn}")
+      @logger.error("Starting position: #{@starting_position.inspect}")
+      @error_queue << error
+    end
+
+    def handle_aws_error(error)
+      error_message = "AWS service error in enhanced fan-out for shard #{@shard_id}: #{error.message} (#{error.class})"
+      @logger.error(error_message)
+      @logger.error("Error code: #{error.code}, Request ID: #{error.request_id}")
+      @error_queue << error
+    end
+
+    def handle_general_error(error)
+      error_message = "Error setting up enhanced fan-out for shard #{@shard_id}: #{error.message} (#{error.class})"
+      @logger.error(error_message)
+      @logger.error(error.backtrace.join("\n")) if error.backtrace
+      @error_queue << error
     end
 
     def process_records(records)
+      @logger.info("Processing #{records.size} records from shard #{@shard_id}")
+
       records.each do |record|
         @record_queue << [@shard_id, record]
       end
